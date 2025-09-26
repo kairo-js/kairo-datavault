6d971aa10f1f1019bbbb085c6ace3fd1713c1aee
shizuku86 <shizuku86710@gmail.com>
2025-09-27 00:36:09 +0900

    Initial commit

---
+++ scripts/Kairo/addons/AddonManager.ts
@@ -0,0 +1,63 @@
+import type { Kairo } from "..";
+import type { AddonProperty } from "./AddonPropertyManager";
+import { system } from "@minecraft/server";
+import { AddonReceiver } from "./router/AddonReceiver";
+
+export type RegistrationState = "registered" | "unregistered" | "missing_requiredAddons";
+
+export interface AddonData {
+    id: string;
+    name: string;
+    description: [string, string];
+    isActive: boolean;
+    isEditable: boolean;
+    selectedVersion: string;
+    activeVersion: string;
+    versions: {
+        [version: string]: {
+            isRegistered: boolean;
+            registrationState: RegistrationState;
+            canInitActivate?: boolean;
+            sessionId?: string;
+            tags?: string[];
+            dependencies?: {
+                module_name: string;
+                version: string;
+            }[];
+            requiredAddons?: {
+                [name: string]: string;
+            };
+        }
+    }
+}
+
+export class AddonManager {
+    private readonly receiver: AddonReceiver;
+
+    private constructor(private readonly kairo: Kairo) {
+        this.receiver = AddonReceiver.create(this);
+    }
+    public static create(kairo: Kairo): AddonManager {
+        return new AddonManager(kairo);
+    }
+
+    public getSelfAddonProperty(): AddonProperty {
+        return this.kairo.getSelfAddonProperty();
+    }
+
+    public subscribeReceiverHooks(): void {
+        system.afterEvents.scriptEventReceive.subscribe(this.receiver.handleScriptEvent);
+    }
+
+    public _activateAddon(): void {
+        this.kairo._activateAddon();
+    }
+
+    public _deactivateAddon(): void {
+        this.kairo._deactivateAddon();
+    }
+
+    public _scriptEvent(message: string): void {
+        this.kairo._scriptEvent(message);
+    }
+}
