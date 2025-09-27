9e01c8b4d6cf79c185cdf28ccdffb0d4f2672cac
shizuku86 <shizuku86710@gmail.com>
2025-09-27 16:53:53 +0900

    first commit

--- README.md
+++ README.md
@@ -1,19 +1,10 @@
-# Kairo-template
-This repository is a template based on the specifications of the Kairo addon linked below.  
-https://github.com/shizuku86/Kairo
+# Kairo-DataVault
+This behavior pack serves as the persistent data vault for the Kairo. It is a dedicated storage module, designed to be immutable and stable across all future versions of Minecraft. Other addons interact with it exclusively via script events to store and retrieve shared data.
 
-After cloning, run the following command to install node_modules:  
-- npm install
-
-After editing the lines ending with # in scripts/properties.ts appropriately and resolving errors,  
-execute the following command in the terminal:  
-- npm run build
-
-When this command is executed, the following operations will be performed:  
-- manifest.json is automatically generated in BP/ and RP/ from the information in properties  
-- TypeScript files in scripts/ are built as JavaScript into BP/scripts  
-- The pack_icon.png at the project root is copied into both BP/ and RP/  
-- The completed BP/ and RP/ are copied into Minecraft’s development folder
+## Supported Minecraft Script API
+Kairo is built using the stable Script API:
+- `@minecraft/server` - v2.1.0
+- `@minecraft/server-ui` - v2.0.0
 
 ## Requirements
 - Node.js (for development and TypeScript build)
--- scripts/properties.ts
+++ scripts/properties.ts
@@ -15,31 +15,31 @@ export type SemVer = {
  */
 
 export const properties = {
-    id: "kairo-template", # // a-z & 0-9 - _
+    id: "kairo-datavault", // a-z & 0-9 - _
     metadata: { 
         /** 製作者の名前 */
         authors: [
-            //"shizuku86"
+            "shizuku86"
         ]
     },
     header: {
-        name: "Kairo-template", #
-        description: "A starter template for developing Minecraft Bedrock addons that rely on Kairo.", #
+        name: "Kairo-DataVault",
+        description: "It is a dedicated storage module, designed to be immutable and stable across all future versions of Minecraft",
         version: { 
             major: 1, 
             minor: 0, 
             patch: 0,
-            // prerelease: "preview.1",
+            prerelease: "dev.1",
             // build: "abc123",
         },
         min_engine_version: [ 1,21,100 ],
-        uuid: "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX" #
+        uuid: "f2d7b2e4-44d9-4b46-bda8-727fb8f848f3"
     },
     resourcepack: {
         name: "Use BP Name",
         description: "Use BP Description",
-        uuid: "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX", #
-        module_uuid: "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX", #
+        uuid: "c839e027-0630-4390-927e-765905300091",
+        module_uuid: "5c39cd64-6cb6-4171-82be-136765cb538d",
     },
     modules: [
         {
@@ -47,17 +47,17 @@ export const properties = {
 			language: "javascript",
 			entry: "scripts/index.js",
             version: "header.version",
-            uuid: "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX" #
+            uuid: "2b2dac89-9772-4a56-8661-e422915aa4e1"
         }
     ],
     dependencies: [
 		{
 			module_name: "@minecraft/server",
-			version: "2.1.0" #
+			version: "2.1.0"
 		},
 		{
 			module_name: "@minecraft/server-ui",
-			version: "2.0.0" #
+			version: "2.0.0"
 		}
 	],
     /** 前提アドオン */
@@ -65,6 +65,7 @@ export const properties = {
         /**
          * id: version (string) // "kairo": "1.0.0"
          */
+        "kairo": "1.0.0-dev.1"
     },
     tags: [
         // "stable",
